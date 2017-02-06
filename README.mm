ead315cfdfe86f2a382e7ac5047192ad7ffd2eb3
Jirka <del@hs-furtwangen.de>
2017-02-06 12:14:00 +0100

    updated readme mindmap

--- README.mm
+++ README.mm
@@ -1,11 +1,14 @@
 <map version="1.0.1">
 <!-- To view this file, download free mind mapping software FreeMind from http://freemind.sourceforge.net -->
 <node CREATED="1486378397436" ID="ID_1426926739" MODIFIED="1486378537951" TEXT="FreeMind Viewer&#xa;Version 0.3.0">
-<node CREATED="1486378444365" FOLDED="true" ID="ID_1518824081" MODIFIED="1486378611715" POSITION="right" TEXT="Usage">
+<node CREATED="1486378444365" ID="ID_1518824081" MODIFIED="1486379039385" POSITION="right" TEXT="Usage">
 <node CREATED="1486378547999" ID="ID_388104812" MODIFIED="1486378561523" TEXT="call index.html"/>
-<node CREATED="1486378562580" FOLDED="true" ID="ID_1243274337" MODIFIED="1486378610994" TEXT="with get-parameters">
+<node CREATED="1486378562580" ID="ID_1243274337" MODIFIED="1486379040836" TEXT="with get-parameters">
 <node CREATED="1486378573125" ID="ID_1277953020" MODIFIED="1486378589504" TEXT="path (path to folder containing the mindmap-file"/>
 <node CREATED="1486378590257" ID="ID_939084055" MODIFIED="1486378604883" TEXT="map (name of the file including extension &quot;.mm&quot;"/>
+</node>
+<node CREATED="1486379042248" ID="ID_848826642" MODIFIED="1486379054635" TEXT="example">
+<node CREATED="1486379054638" ID="ID_492338000" MODIFIED="1486379098336" TEXT="..../index.html?path=.&amp;map=README.mm"/>
 </node>
 </node>
 </node>
