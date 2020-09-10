c4c3b282be85d0ef262956810d6a096216fc9fbe
Jirka Dell'Oro-Friedl <del@hs-furtwangen.de>
2020-09-10 14:29:49 +0200

    update file

---
+++ Test/test.mm
@@ -0,0 +1,14 @@
+<node><node CREATED="1486378397436" ID="ID_1426926739" MODIFIED="1486378537951" TEXT="FreeMind Viewer&#10;Version 0.3.0">
+<node CREATED="1486378444365" ID="ID_1518824081" MODIFIED="1486379039385" POSITION="right" TEXT="Usage" FOLDED="false">
+<node CREATED="1486378547999" ID="ID_388104812" MODIFIED="1486378561523" TEXT="call index.html" POSITION="right" FOLDED="false"/>
+
+<node CREATED="1486379042248" ID="ID_848826642" MODIFIED="1486379054635" TEXT="example" POSITION="right" FOLDED="false">
+<node CREATED="1486379054638" ID="ID_492338000" MODIFIED="1486379098336" TEXT="..../index.html?path=.&amp;map=README.mm&amp;list=false" POSITION="right" FOLDED="false"/>
+<node CREATED="1486379054638" ID="ID_492338000" MODIFIED="1486379098336" TEXT="shows map &quot;README&quot; in mm folder as mindmap" POSITION="right" FOLDED="false"/>
+</node>
+</node>
+<node CREATED="1486378562580" ID="ID_1243274337" MODIFIED="1486379040836" TEXT="with get-parameters" POSITION="left" FOLDED="false">
+<node CREATED="1486378573125" ID="ID_1277953020" MODIFIED="1486378589504" TEXT="path (path to folder containing the mindmap-file" POSITION="left" FOLDED="false"/>
+<node CREATED="1486378590257" ID="ID_939084055" MODIFIED="1486378604883" TEXT="map (name of the file including extension &quot;.mm&quot;" POSITION="left" FOLDED="false"/>
+<node CREATED="1486378590257" ID="ID_939084055" MODIFIED="1486378604883" TEXT="list (boolean to show data as list or as mindmap)" POSITION="left" FOLDED="false"/>
+</node></node></node>
